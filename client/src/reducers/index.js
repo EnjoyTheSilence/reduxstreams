21dfacd1b6b4f146ac54ee6841fd043f40153a78
Sswingle <seiji.swingl@gmail.com>
2019-02-01 10:36:19 -0800

    created dummy backend using dbjson. finished error handling on stream create

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+//can also import reducer as import { reducer } from 'redux-form'
 import authReducer from './authReducer';
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 })
