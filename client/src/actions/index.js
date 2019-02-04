b0aade9dde410388ac7d38e4a48449172a3c72ff
Sswingle <seiji.swingl@gmail.com>
2019-02-04 13:31:05 -0800

    delete stream component complete. refactored modal component to be reusable

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -59,5 +59,6 @@ export const deleteStream = (id) => async dispatch => {
   //no response
   await streams.delete(`/streams/${id}`);
 
-  dispatch({ type: DELETE_STREAM , payload: id })
+  dispatch({ type: DELETE_STREAM , payload: id });
+  history.push('/');
 }
--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
 const Modal = props => {
   return ReactDOM.createPortal(
     <div
-      onClick={()=>history.push('/')}
+      onClick={props.onDismiss}
       className="ui dimmer modals visible active"
     >
       <div
@@ -13,20 +12,9 @@ const Modal = props => {
           //stops event from bubbling up and being called when we click on modal box
         className="ui standard modal visible active"
       >
-        <div className="header">
-          {props.title}
-        </div>
-        <div className="content">
-          Are you sure you want to delete this stream?
-        </div>
-        <div className="actions">
-          <button className="ui primary button">
-            Delete
-          </button>
-          <button className="ui button" >
-            Cancel
-          </button>
-        </div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector('#modal')
